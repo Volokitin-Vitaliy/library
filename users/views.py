2dda5a565dd52d7c939d5474d4bde5726f67f440
Fibonacci <volokitin.vitaliy@icloud.com>
2025-11-19 08:14:10 +0200

    Add register and me API views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from users.serializers import UserRegisterSerializer, MeSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = MeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
